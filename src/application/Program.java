65258e8ea496dd3a9cea7f96c18aa24e3c399ccb
RitaFer <rialf.ferreira@gmail.com>
2022-08-16 23:47:29 -0300

    insert data and return generated-id

--- src/application/Program.java
+++ src/application/Program.java
@@ -9,19 +9,27 @@ import java.text.SimpleDateFormat;
 public class Program {
     public static void main(String[] args) {
         SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
-        Connection conn = null;
+        Connection conn;
         PreparedStatement st = null;
 
         try {
             conn = DB.getConnection();
-            st = conn.prepareStatement("INSERT INTO seller (Name, Email, BirthDate, BaseSalary, DepartmentId) VALUES (?, ?, ?, ?, ?)");
+            st = conn.prepareStatement("INSERT INTO seller (Name, Email, BirthDate, BaseSalary, DepartmentId) VALUES (?, ?, ?, ?, ?)", Statement.RETURN_GENERATED_KEYS);
             st.setString(1, "Rita Ferreira");
             st.setString(2, "rialf.ferreira@gmail.com");
             st.setDate(3, new java.sql.Date(sdf.parse("26/07/1999").getTime()));
             st.setDouble(4, 3000.0);
             st.setInt(5, 4);
             int rowAffected = st.executeUpdate();
-            System.out.println("Foram afetadas "+rowAffected+" linhas.");
+            if (rowAffected > 0) {
+                ResultSet rs = st.getGeneratedKeys();
+                while(rs.next()){
+                    int id = rs.getInt(1);
+                    System.out.println("Feito! Id="+id);
+                }
+            } else {
+                System.out.println("Nenhuma linha foi afetada");
+            }
         } catch (SQLException | ParseException e){
             e.printStackTrace();
         } finally {
