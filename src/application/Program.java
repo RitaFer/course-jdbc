3ff8691ead7a623b737cd64f89e286d5b4722204
RitaFer <rialf.ferreira@gmail.com>
2022-08-16 23:40:42 -0300

    insert data

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,29 +1,30 @@
 package application;
 
 import db.DB;
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+
+import java.sql.*;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 
 public class Program {
     public static void main(String[] args) {
-
-        Connection conn;
-        Statement st = null;
-        ResultSet rs = null;
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+        Connection conn = null;
+        PreparedStatement st = null;
 
         try {
             conn = DB.getConnection();
-            st = conn.createStatement();
-            rs = st.executeQuery("select * from department");
-            while(rs.next()){
-                System.out.println(rs.getInt("Id")+", "+rs.getString("Name"));
-            }
-        } catch (SQLException e){
+            st = conn.prepareStatement("INSERT INTO seller (Name, Email, BirthDate, BaseSalary, DepartmentId) VALUES (?, ?, ?, ?, ?)");
+            st.setString(1, "Rita Ferreira");
+            st.setString(2, "rialf.ferreira@gmail.com");
+            st.setDate(3, new java.sql.Date(sdf.parse("26/07/1999").getTime()));
+            st.setDouble(4, 3000.0);
+            st.setInt(5, 4);
+            int rowAffected = st.executeUpdate();
+            System.out.println("Foram afetadas "+rowAffected+" linhas.");
+        } catch (SQLException | ParseException e){
             e.printStackTrace();
         } finally {
-            DB.closeResultSet(rs);
             DB.closeStatement(st);
             DB.closeConnection();
         }
